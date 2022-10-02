b9eda98404add59f8072cafb8ba447a6c3d1c583
Apwin98 <67622001+Apwin98@users.noreply.github.com>
2022-10-02 04:37:52 -0500

    roughdraft

--- dist/js/canvas.bundle.js
+++ dist/js/canvas.bundle.js
@@ -86,6 +86,110 @@
 /************************************************************************/
 /******/ ({
 
+/***/ "./src/img/Boat_Door.png":
+/*!*******************************!*\
+  !*** ./src/img/Boat_Door.png ***!
+  \*******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = (__webpack_require__.p + "79b47e11331b8fc4b7efa0d274eea5fd.png");
+
+/***/ }),
+
+/***/ "./src/img/Floor_Background.png":
+/*!**************************************!*\
+  !*** ./src/img/Floor_Background.png ***!
+  \**************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = (__webpack_require__.p + "42315be03468dead7b21122fa15d39ae.png");
+
+/***/ }),
+
+/***/ "./src/img/Jake_Standing.png":
+/*!***********************************!*\
+  !*** ./src/img/Jake_Standing.png ***!
+  \***********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = (__webpack_require__.p + "727e1a02761c8d788e5af4c7639e7f26.png");
+
+/***/ }),
+
+/***/ "./src/img/Pet_Door.png":
+/*!******************************!*\
+  !*** ./src/img/Pet_Door.png ***!
+  \******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = (__webpack_require__.p + "e14709610b068ff5926d8ab030653386.png");
+
+/***/ }),
+
+/***/ "./src/img/Single_Cloud.png":
+/*!**********************************!*\
+  !*** ./src/img/Single_Cloud.png ***!
+  \**********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = (__webpack_require__.p + "07166c0ee4fab004998668cf5bc420af.png");
+
+/***/ }),
+
+/***/ "./src/img/Sky2.png":
+/*!**************************!*\
+  !*** ./src/img/Sky2.png ***!
+  \**************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = (__webpack_require__.p + "f3012e50cc22d7117dd3f6d81c0ef355.png");
+
+/***/ }),
+
+/***/ "./src/img/Sky_Background.png":
+/*!************************************!*\
+  !*** ./src/img/Sky_Background.png ***!
+  \************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = (__webpack_require__.p + "0a1ff426d53b1c7ed9148ab858987298.png");
+
+/***/ }),
+
+/***/ "./src/img/State_Farm.png":
+/*!********************************!*\
+  !*** ./src/img/State_Farm.png ***!
+  \********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = (__webpack_require__.p + "1917d39f0b32039701c6543ad58a2855.png");
+
+/***/ }),
+
 /***/ "./src/js/canvas.js":
 /*!**************************!*\
   !*** ./src/js/canvas.js ***!
@@ -95,8 +199,14 @@
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./utils */ "./src/js/utils.js");
-/* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_utils__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _img_Single_Cloud_png__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../img/Single_Cloud.png */ "./src/img/Single_Cloud.png");
+/* harmony import */ var _img_Floor_Background_png__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../img/Floor_Background.png */ "./src/img/Floor_Background.png");
+/* harmony import */ var _img_Sky_Background_png__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../img/Sky_Background.png */ "./src/img/Sky_Background.png");
+/* harmony import */ var _img_Sky2_png__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../img/Sky2.png */ "./src/img/Sky2.png");
+/* harmony import */ var _img_State_Farm_png__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../img/State_Farm.png */ "./src/img/State_Farm.png");
+/* harmony import */ var _img_Pet_Door_png__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../img/Pet_Door.png */ "./src/img/Pet_Door.png");
+/* harmony import */ var _img_Boat_Door_png__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../img/Boat_Door.png */ "./src/img/Boat_Door.png");
+/* harmony import */ var _img_Jake_Standing_png__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ../img/Jake_Standing.png */ "./src/img/Jake_Standing.png");
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -104,105 +214,323 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
 
+
+
+
+
+
+
+
 var canvas = document.querySelector('canvas');
 var c = canvas.getContext('2d');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
-var mouse = {
-  x: innerWidth / 2,
-  y: innerHeight / 2
-};
-var colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']; // Event Listeners
+var gravity = 0.5;
 
-addEventListener('mousemove', function (event) {
-  mouse.x = event.clientX;
-  mouse.y = event.clientY;
-});
-addEventListener('resize', function () {
-  canvas.width = innerWidth;
-  canvas.height = innerHeight;
-  init();
-}); // Objects
+var Player = /*#__PURE__*/function () {
+  function Player() {
+    _classCallCheck(this, Player);
 
-var _Object = /*#__PURE__*/function () {
-  function Object(x, y, radius, color) {
-    _classCallCheck(this, Object);
-
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-    this.color = color;
+    this.position = {
+      x: 50,
+      y: 100
+    };
+    this.velocity = {
+      x: 0,
+      y: 1
+    };
+    this.width = 250;
+    this.height = 200;
+    this.image = createImage(_img_Jake_Standing_png__WEBPACK_IMPORTED_MODULE_7__["default"]);
   }
 
-  _createClass(Object, [{
+  _createClass(Player, [{
     key: "draw",
     value: function draw() {
-      c.beginPath();
-      c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-      c.fillStyle = this.color;
-      c.fill();
-      c.closePath();
+      c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);
     }
   }, {
     key: "update",
     value: function update() {
       this.draw();
+      this.position.x += this.velocity.x;
+      this.position.y += this.velocity.y;
+      if (this.position.y + this.height + this.velocity.y <= canvas.height) this.velocity.y += gravity;else this.velocity.y = 0;
     }
   }]);
 
-  return Object;
-}(); // Implementation
+  return Player;
+}(); //ORIGINAL
 
 
-var objects;
+var Platform = /*#__PURE__*/function () {
+  function Platform(_ref) {
+    var x = _ref.x,
+        y = _ref.y,
+        image = _ref.image;
 
-function init() {
-  objects = [];
+    _classCallCheck(this, Platform);
 
-  for (var i = 0; i < 400; i++) {// objects.push()
+    this.position = {
+      x: x,
+      y: y
+    };
+    this.image = image;
+    this.width = image.width;
+    this.height = image.height;
   }
-} // Animation Loop
 
+  _createClass(Platform, [{
+    key: "draw",
+    value: function draw() {
+      c.drawImage(this.image, this.position.x, this.position.y);
+    }
+  }]);
+
+  return Platform;
+}(); // COPY
+
+
+var GenericObject = /*#__PURE__*/function () {
+  function GenericObject(_ref2) {
+    var x = _ref2.x,
+        y = _ref2.y,
+        image = _ref2.image;
+
+    _classCallCheck(this, GenericObject);
+
+    this.position = {
+      x: x,
+      y: y
+    };
+    this.image = image;
+    this.width = image.width;
+    this.height = image.height;
+  }
+
+  _createClass(GenericObject, [{
+    key: "draw",
+    value: function draw() {
+      c.drawImage(this.image, this.position.x, this.position.y);
+    }
+  }]);
+
+  return GenericObject;
+}();
+
+var Doors = /*#__PURE__*/function () {
+  function Doors(_ref3) {
+    var x = _ref3.x,
+        y = _ref3.y,
+        image = _ref3.image;
+
+    _classCallCheck(this, Doors);
+
+    this.position = {
+      x: x,
+      y: y
+    };
+    this.image = image;
+    this.width = image.width;
+    this.height = image.height;
+  }
+
+  _createClass(Doors, [{
+    key: "draw",
+    value: function draw() {
+      c.drawImage(this.image, this.position.x, this.position.y);
+    }
+  }]);
+
+  return Doors;
+}(); // ORIGINAL
+
+
+function createImage(imageSrc) {
+  var image = new Image();
+  image.src = imageSrc;
+  return image;
+}
+
+var platformImage = createImage(_img_Floor_Background_png__WEBPACK_IMPORTED_MODULE_1__["default"]);
+var player = new Player(); //const platform = new Platform()
+
+var platforms = [new Platform({
+  x: -1,
+  y: 700,
+  image: platformImage
+}), new Platform({
+  x: platformImage.width - 1,
+  y: 700,
+  image: platformImage
+}), new Platform({
+  x: 0,
+  y: 700,
+  image: platformImage
+}), new Platform({
+  x: 700,
+  y: -900,
+  image: createImage(_img_State_Farm_png__WEBPACK_IMPORTED_MODULE_4__["default"])
+})];
+var dogDoor = [new Doors({
+  x: -800,
+  y: -785,
+  image: createImage(_img_Pet_Door_png__WEBPACK_IMPORTED_MODULE_5__["default"])
+}), new Doors({
+  x: -200,
+  y: -785,
+  image: createImage(_img_Boat_Door_png__WEBPACK_IMPORTED_MODULE_6__["default"])
+})];
+var genericObjects = [new GenericObject({
+  x: 0,
+  y: 0,
+  image: createImage(_img_Sky_Background_png__WEBPACK_IMPORTED_MODULE_2__["default"])
+}), new GenericObject({
+  x: createImage(_img_Sky_Background_png__WEBPACK_IMPORTED_MODULE_2__["default"]).width - 1,
+  y: 0,
+  image: createImage(_img_Sky_Background_png__WEBPACK_IMPORTED_MODULE_2__["default"])
+}), new GenericObject({
+  x: 0,
+  y: -500,
+  image: createImage(_img_Single_Cloud_png__WEBPACK_IMPORTED_MODULE_0__["default"])
+}), new GenericObject({
+  x: -800,
+  y: -500,
+  image: createImage(_img_Single_Cloud_png__WEBPACK_IMPORTED_MODULE_0__["default"])
+}), new GenericObject({
+  x: -1200,
+  y: -450,
+  image: createImage(_img_Sky2_png__WEBPACK_IMPORTED_MODULE_3__["default"])
+}), new GenericObject({
+  x: -500,
+  y: -400,
+  image: createImage(_img_Sky2_png__WEBPACK_IMPORTED_MODULE_3__["default"])
+}) // new GenericObject({
+//   x:-800,
+//   y:-785,
+//   image: createImage(dog_door)
+// }),
+// new GenericObject({
+//   x:-200,
+//   y:-785,
+//   image: createImage(boat_door)
+// }),
+];
+var keys = {
+  right: {
+    pressed: false
+  },
+  left: {
+    pressed: false
+  }
+};
+var scrollOffset = 0;
 
 function animate() {
   requestAnimationFrame(animate);
+  c.fillStyle = 'white';
   c.clearRect(0, 0, canvas.width, canvas.height);
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y); // objects.forEach(object => {
-  //  object.update()
-  // })
+  genericObjects.forEach(function (genericObject) {
+    genericObject.draw();
+  });
+  platforms.forEach(function (platform) {
+    platform.draw();
+  });
+  dogDoor.forEach(function (Doors) {
+    Doors.draw();
+  });
+  player.update();
+
+  if (keys.right.pressed && player.position.x < 400) {
+    player.velocity.x = 5;
+  } else if (keys.left.pressed && player.position.x > 100 || keys.left.pressed && scrollOffset === 0 && player.position.x > 0) {
+    player.velocity.x = -5;
+  } else {
+    player.velocity.x = 0;
+
+    if (keys.right.pressed) {
+      scrollOffset += 5;
+      platforms.forEach(function (platform) {
+        platform.position.x -= 5;
+      });
+      genericObjects.forEach(function (genericObject) {
+        genericObject.position.x -= 3;
+      });
+      dogDoor.forEach(function (Doors) {
+        Doors.position.x -= 5;
+      });
+    } else if (keys.left.pressed && scrollOffset > 0) {
+      scrollOffset -= 5;
+      platforms.forEach(function (platform) {
+        platform.position.x += 5;
+      });
+      genericObjects.forEach(function (genericObject) {
+        genericObject.position.x += 3;
+      });
+      dogDoor.forEach(function (Doors) {
+        Doors.position.x += 5;
+      });
+    }
+  } //Detect collusion with cloud (blue rectangle)
+
+
+  platforms.forEach(function (platform) {
+    if (player.position.y + player.height <= platform.position.y && player.position.y + player.height + player.velocity.y >= platform.position.y && player.position.x + player.width >= platform.position.x && player.position.x <= platform.position.x + platform.width) {
+      player.velocity.y = 0;
+    }
+  });
 }
 
-init();
 animate();
+addEventListener('keydown', function (_ref4) {
+  var keyCode = _ref4.keyCode;
 
-/***/ }),
+  switch (keyCode) {
+    case 65:
+      console.log('left');
+      keys.left.pressed = true;
+      break;
 
-/***/ "./src/js/utils.js":
-/*!*************************!*\
-  !*** ./src/js/utils.js ***!
-  \*************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
+    case 83:
+      console.log('down');
+      break;
 
-function randomIntFromRange(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
+    case 68:
+      console.log('right');
+      keys.right.pressed = true;
+      break;
 
-function randomColor(colors) {
-  return colors[Math.floor(Math.random() * colors.length)];
-}
+    case 87:
+      console.log('up');
+      player.velocity.y -= 10;
+      window.location.href = "https://google.com";
+      break;
+  }
+});
+addEventListener('keyup', function (_ref5) {
+  var keyCode = _ref5.keyCode;
 
-function distance(x1, y1, x2, y2) {
-  var xDist = x2 - x1;
-  var yDist = y2 - y1;
-  return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
-}
+  switch (keyCode) {
+    case 65:
+      console.log('left');
+      keys.left.pressed = false;
+      break;
 
-module.exports = {
-  randomIntFromRange: randomIntFromRange,
-  randomColor: randomColor,
-  distance: distance
-};
+    case 83:
+      console.log('down');
+      break;
+
+    case 68:
+      console.log('right');
+      keys.right.pressed = false;
+      break;
+
+    case 87:
+      console.log('up');
+      keys.right.pressed = false;
+      break;
+  }
+});
 
 /***/ })
 
--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,73 +1,305 @@
-import utils from './utils'
-
+import cloud from '../img/Single_Cloud.png'
+import platform from '../img/Floor_Background.png'
+import sky from '../img/Sky_Background.png'
+import cloud2 from '../img/Sky2.png'
+import building from '../img/State_Farm.png'
+import dog_door from '../img/Pet_Door.png'
+import boat_door from '../img/Boat_Door.png'
+import jake from '../img/Jake_Standing.png'
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
 canvas.width = innerWidth
 canvas.height = innerHeight
 
-const mouse = {
-  x: innerWidth / 2,
-  y: innerHeight / 2
+const gravity = 0.5
+class Player{
+    constructor(){
+        this.position = {
+            x:50,
+            y:100
+        }
+        this.velocity = {
+            x:0,
+            y:1
+        }
+        this.width = 250
+        this.height = 200
+
+        this.image = createImage(jake)
+     }
+        draw(){
+            c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
+            
+            
+    }
+    
+    update(){
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+        if (this.position.y + this.height + this.velocity.y <= canvas.height)
+        this.velocity.y += gravity
+        else this.velocity.y = 0
+    }
+
 }
 
-const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
+//ORIGINAL
 
-// Event Listeners
-addEventListener('mousemove', (event) => {
-  mouse.x = event.clientX
-  mouse.y = event.clientY
-})
+class Platform {
+    constructor({x, y, image}){
+        this.position = {
+            x:x,
+            y:y
+        }
+        this.image = image
+        this.width= image.width
+        this.height= image.height
+        
+    }
+    draw(){
+        c.drawImage(this.image, this.position.x, this.position.y)
+    }
+}
 
-addEventListener('resize', () => {
-  canvas.width = innerWidth
-  canvas.height = innerHeight
 
-  init()
-})
 
-// Objects
-class Object {
-  constructor(x, y, radius, color) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
+// COPY
+
+class GenericObject{
+  constructor({x, y, image}){
+      this.position = {
+          x:x,
+          y:y
+      }
+      this.image = image
+      this.width= image.width
+      this.height= image.height
+      
   }
-
-  draw() {
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
-    c.closePath()
-  }
-
-  update() {
-    this.draw()
+  draw(){
+      c.drawImage(this.image, this.position.x, this.position.y)
   }
 }
 
-// Implementation
-let objects
-function init() {
-  objects = []
-
-  for (let i = 0; i < 400; i++) {
-    // objects.push()
+class Doors {
+  constructor({x,y, image}){
+    this.position ={
+      x: x,
+      y: y,
+    }
+    this.image = image
+    this.width = image.width
+    this.height = image.height
+  }
+  draw()
+  {
+    c.drawImage(this.image, this.position.x, this.position.y)
   }
 }
 
-// Animation Loop
+
+// ORIGINAL
+
+function createImage(imageSrc)
+{
+  const image = new Image()
+  image.src = imageSrc
+  return image
+}
+
+const platformImage = createImage(platform)
+
+const player = new Player()
+//const platform = new Platform()
+const platforms = [
+  new Platform({
+    x:-1, 
+    y:700, 
+    image: platformImage
+  }), 
+  new Platform({x: platformImage.width-1, y:700,image: platformImage}), new Platform({x: 0, y:700,image: platformImage}),
+  new Platform({x: 700, y:-900,image: createImage(building)})
+]
+
+const dogDoor = [
+  new Doors({
+    x:-800,
+    y:-785,
+    image: createImage(dog_door)
+  }),
+  new Doors({
+    x:-200,
+    y:-785,
+    image: createImage(boat_door)
+  }),
+]
+
+const genericObjects = [
+  new GenericObject({
+    x:0,
+    y:0,
+    image: createImage(sky)
+  }),
+  new GenericObject({
+    x: createImage(sky).width-1,
+    y:0,
+    image: createImage(sky)
+  }),
+  new GenericObject({
+    x:0,
+    y:-500,
+    image: createImage(cloud)
+  }),
+  new GenericObject({
+    x:-800,
+    y:-500,
+    image: createImage(cloud)
+  }),
+  new GenericObject({
+    x:-1200,
+    y:-450,
+    image: createImage(cloud2)
+  }),
+  new GenericObject({
+    x:-500,
+    y:-400,
+    image: createImage(cloud2)
+  })
+  // new GenericObject({
+  //   x:-800,
+  //   y:-785,
+  //   image: createImage(dog_door)
+  // }),
+  // new GenericObject({
+  //   x:-200,
+  //   y:-785,
+  //   image: createImage(boat_door)
+  // }),
+]
+
+const keys = {
+    right: {
+        pressed: false
+    },
+    left: {
+        pressed: false
+    }
+}
+
+let scrollOffset = 0
+
 function animate() {
-  requestAnimationFrame(animate)
-  c.clearRect(0, 0, canvas.width, canvas.height)
+    requestAnimationFrame(animate)
+    c.fillStyle='white'
+    c.clearRect(0, 0, canvas.width, canvas.height)
+  
+    genericObjects.forEach(genericObject =>{
+        genericObject.draw()
+    })
+    platforms.forEach(platform => {
+        platform.draw()
+    })
+    dogDoor.forEach(Doors =>{
+        Doors.draw()
+    })
+    player.update()
+ 
 
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
-  // objects.forEach(object => {
-  //  object.update()
-  // })
+    if (keys.right.pressed && player.position.x < 400) {
+        player.velocity.x = 5
+    }else if ((keys.left.pressed && player.position.x > 100) || (keys.left.pressed && scrollOffset === 0 && player.position.x > 0)) {
+        player.velocity.x = -5
+    }else {
+        player.velocity.x = 0
+        if (keys.right.pressed)
+        {
+            scrollOffset +=5
+            platforms.forEach(platform => {
+                platform.position.x -= 5
+            })
+            genericObjects.forEach(genericObject => {
+              genericObject.position.x -= 3
+            })
+            dogDoor.forEach(Doors => {
+              Doors.position.x -= 5
+            })
+            
+        }else if (keys.left.pressed && scrollOffset > 0){
+          scrollOffset -=5
+            platforms.forEach(platform => {
+                platform.position.x +=5
+            })
+            genericObjects.forEach(genericObject => {
+              genericObject.position.x += 3
+            })
+            dogDoor.forEach(Doors => {
+              Doors.position.x += 5
+            })
+            
+        }
+    }
+
+
+
+    //Detect collusion with cloud (blue rectangle)
+    platforms.forEach(platform => {
+    
+        if(player.position.y+player.height <= platform.position.y && player.position.y + player.height + 
+            player.velocity.y >= platform.position.y && player.position.x + player.width >= platform.position.x && player.position.x <= platform.position.x +platform.width)
+        {
+            player.velocity.y = 0 
+        }
+    })
 }
 
-init()
+
 animate()
+
+addEventListener('keydown', ({ keyCode }) => {
+    switch (keyCode) {
+        case 65:
+            console.log('left')
+            keys.left.pressed = true
+            break
+
+        case 83:
+            console.log('down')
+            break
+
+        case 68:
+            console.log('right')
+            keys.right.pressed = true
+            break
+        case 87:
+            console.log('up')
+            player.velocity.y -= 10
+            window.location.href = "https://google.com"
+            break
+
+    }
+})
+
+addEventListener('keyup', ({ keyCode }) => {
+    switch (keyCode) {
+        case 65:
+            console.log('left')
+            keys.left.pressed = false
+            break
+
+        case 83:
+            console.log('down')
+            break
+
+        case 68:
+            console.log('right')
+            keys.right.pressed = false
+            break
+
+        case 87:
+            console.log('up')
+            keys.right.pressed = false
+            break
+    }
+})
